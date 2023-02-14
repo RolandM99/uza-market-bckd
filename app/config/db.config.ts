08c433bc3a1a73721215aa784acaccb8988b73be
rolandm99 <manfulmweze99@gmail.com>
2023-02-14 12:41:09 +0200

    Create the MVC backbone of the project

---
+++ app/config/db.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "roland",
+    PASSWORD: "man1234",
+    DB: "game_time_db",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
